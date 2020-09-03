57b41f67d6ad89ffe0eaa82ad38fdf290d68e21e
Nikita Moiseienko <moiseiandko@gmail.com>
2020-09-03 21:40:50 +0300

    ]

--- src/main/java/locators/GroupsSearchLoc.java
+++ src/main/java/locators/GroupsSearchLoc.java
@@ -3,7 +3,7 @@ package locators;
 import org.openqa.selenium.By;
 
 public enum GroupsSearchLoc implements Locator {
-    SEE_ALL_BUTTON(By.cssSelector("[aria-label='See all'")),
+    SEE_ALL_BUTTON(By.cssSelector("[aria-label='See all']")),
     END_OF_RESULTS_LABEL(By.cssSelector("div.l29c1vbm")),
     RESULT_LI(By.cssSelector("div.sjgh65i0.tr9rh885"));
 
